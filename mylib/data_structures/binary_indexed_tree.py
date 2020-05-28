e33c46e09c2f08868895630a35ebc394c645d1af
mikelty <michaellty2520@gmail.com>
2020-05-28 09:45:12 -0500

    fixed bit

--- mylib/data_structures/binary_indexed_tree.py
+++ mylib/data_structures/binary_indexed_tree.py
@@ -5,38 +5,41 @@ class BIT():
     both operations require O(logn) time
     inspired by https://en.wikipedia.org/wiki/Fenwick_tree
     """
-    def __init__(self,length=0):
+
+    def __init__(self, length=0):
         """
-        note this implementation uses one-based indexing
+        note this implementation uses one-based indexing,
+        but all functions uses 0-based indexing as arguments.
         :param length: length of the array
         """
-        self.length=length
-        self.A=[0]*(self.length+1)
+        self.A = [0] * (length + 1)
 
-    def LSB(self,i):
+    def LSB(self, i):
         """
         get least significant bit
         :return: sum of the first i elements, including the i-th element.
         """
         return i & -i
 
-    def sum(self,i):
+    def sum(self, i):
         """
-        :param i: i-th element
+        :param i: i-th element, 0 based indexing.
         :return: sum of the first i elements, including the i-th element.
         """
-        res=0
-        while i>0:
-            res+=self.A[i]
-            i-=self.LSB(i)
+        res,i = 0,i+1
+        while i > 0:
+            res += self.A[i]
+            i -= self.LSB(i)
         return res
 
-    def add(self,i,a):
+    def add(self, i, a):
         """
         add a to the i-th element
-        :param i: i-th element
+        :param i: i-th element, 0-based index.
         :param a: amount added
         """
-        while i<=len(self.A):
-            self.A[i]+=a
-            i+=self.LSB(i)
+        i+=1
+        while i < len(self.A):
+            self.A[i] += a
+            i += self.LSB(i)
+
